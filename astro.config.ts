7b54d464ed3026276703408e8e561c413dc6a460
Chris Kerr <chriskerr@me.com>
2023-04-24 10:20:19 +1000

    add vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import image from "@astrojs/image";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [image()],
+  adapter: vercel(),
 });
