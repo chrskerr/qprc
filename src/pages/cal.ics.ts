671f762efaf6e7090245c79f4c6a7aa1d47acad6
Chris Kerr <chriskerr@me.com>
2023-04-25 18:55:21 +1000

    fix failed build

--- src/pages/cal.ics.ts
+++ src/pages/cal.ics.ts
@@ -1,34 +1,42 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
-import { EventAttributes, createEvents } from "ics";
+import { EventAttributes, ReturnObject, createEvents } from "ics";
 import { getIcsDateArray } from "../lib/dates";
 import { longTitle } from "../lib/constants";
 import { meetingPoints } from "../lib/meetingPoints";
 
-const runs = await getCollection("runs");
+let events: ReturnObject;
 
-const events = createEvents(
-  runs
-    .map<EventAttributes | null>((run) => {
-      const meetingPoint = meetingPoints[run.data.meetingPoint];
-      if (!meetingPoint) return null;
+async function generateIcs() {
+  const runs = await getCollection("runs");
 
-      return {
-        uid: run.slug,
-        start: getIcsDateArray(run.data.startTime),
-        startInputType: "utc",
-        startOutputType: "utc",
-        duration: { hours: 2 },
-        status: "CONFIRMED",
-        title: `RUN: ${longTitle}`,
-        location: meetingPoint.location,
-      };
-    })
-    .filter((event): event is EventAttributes => !!event)
-);
+  return createEvents(
+    runs
+      .map<EventAttributes | null>((run) => {
+        const meetingPoint = meetingPoints[run.data.meetingPoint];
+        if (!meetingPoint) return null;
+
+        return {
+          uid: run.slug,
+          start: getIcsDateArray(run.data.startTime),
+          startInputType: "utc",
+          startOutputType: "utc",
+          duration: { hours: 2 },
+          status: "CONFIRMED",
+          title: `RUN: ${longTitle}`,
+          location: meetingPoint.location,
+        };
+      })
+      .filter((event): event is EventAttributes => !!event)
+  );
+}
 
 export const get: APIRoute = async () => {
-  return new Response(events.value, {
+  if (!events) {
+    events = await generateIcs();
+  }
+
+  return new Response(events?.value, {
     headers: new Headers({ "content-type": "text/calendar" }),
   });
 };
