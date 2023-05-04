81784744dcbffa6064f8c1de6438c5755ba20dc9
Chris Kerr <chriskerr@me.com>
2023-05-05 09:32:18 +1000

    big card styling update

--- src/lib/meetingPoints.ts
+++ src/lib/meetingPoints.ts
@@ -1,7 +1,10 @@
-export const meetingPoints: Record<
-  string,
-  { embedUrl: string; appleUrl: string; location: string }
-> = {
+export type MeetingPoint = {
+  embedUrl: string;
+  appleUrl: string;
+  location: string;
+};
+
+export const meetingPoints: Record<string, MeetingPoint> = {
   "alex-surf-club": {
     embedUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14262.160784757805!2d153.09718785!3d-26.663200600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b937676385a8caf%3A0x39ed56a1cd495557!2sAlex%20Surf%20Club!5e0!3m2!1sen!2sau!4v1682303201377!5m2!1sen!2sau",
--- src/lib/runs.ts
+++ src/lib/runs.ts
@@ -1,20 +1,36 @@
 import { CollectionEntry, getCollection } from "astro:content";
 
-export async function getUpcomingRuns(
-  sort: "asc" | "desc" = "asc"
-): Promise<CollectionEntry<"runs">[]> {
-  const now = Date.now();
+type Run = CollectionEntry<"runs">;
+type Runs = Run[];
 
-  const allRuns = await getCollection("runs");
-  const filteredRuns = allRuns.filter((run) => {
-    return run.data.startTime.valueOf() > now;
-  });
-
-  return filteredRuns.sort((a, b) => {
+function createSorter(sort: "asc" | "desc") {
+  return function (a: Run, b: Run): number {
     if (sort === "asc") {
       return a.data.startTime.valueOf() - b.data.startTime.valueOf();
     } else {
       return b.data.startTime.valueOf() - a.data.startTime.valueOf();
     }
-  });
+  };
+}
+
+export async function getSortedRuns(): Promise<{ upcoming: Runs; past: Runs }> {
+  const now = Date.now();
+
+  const allRuns = await getCollection("runs");
+
+  const upcomingRuns: Runs = [];
+  const pastRuns: Runs = [];
+
+  for (const run of allRuns) {
+    if (run.data.startTime.valueOf() > now) {
+      upcomingRuns.push(run);
+    } else {
+      pastRuns.push(run);
+    }
+  }
+
+  return {
+    upcoming: upcomingRuns.sort(createSorter("asc")),
+    past: pastRuns.sort(createSorter("desc")),
+  };
 }
