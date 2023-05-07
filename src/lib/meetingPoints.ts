a30e09b60ed79db9f762259ca1d277c99658cf40
Chris Kerr <chriskerr@me.com>
2023-05-08 09:03:41 +1000

    replaced embed with pictures

--- src/lib/meetingPoints.ts
+++ src/lib/meetingPoints.ts
@@ -1,24 +1,28 @@
+import alexHotel from "../images/meetingPoints/alex-hotel.png?w=200;400;800;1600&format=avif;webp&as=picture";
+import alexSurfClub from "../images/meetingPoints/alex-surf-club.png?w=200;400;800;1600&format=avif;webp&as=picture";
+
 export type MeetingPoint = {
-  embedUrl: string;
+  googleUrl: string;
   appleUrl: string;
   location: string;
+  image: PictureData;
 };
 
 export const meetingPoints: Record<string, MeetingPoint> = {
   "alex-surf-club": {
-    embedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14262.160784757805!2d153.09718785!3d-26.663200600000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b937676385a8caf%3A0x39ed56a1cd495557!2sAlex%20Surf%20Club!5e0!3m2!1sen!2sau!4v1682303201377!5m2!1sen!2sau",
+    googleUrl: "https://goo.gl/maps/aVbKhasF9ewbjdcM8",
     appleUrl:
       "https://maps.apple.com/?address=167%20Alexandra%20Pde,%20Alexandra%20Headland%20QLD%204572,%20Australia&auid=11155189744450364328&ll=-26.669116,153.107494&lsp=9902&q=Alex%20Surf%20Club.",
     location:
       "Alex Surf Club, 167 Alexandra Parade, Alexandra Headland QLD 4572",
+    image: alexSurfClub,
   },
   "alex-hotel": {
-    embedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3565.5282128169433!2d153.10232477698187!3d-26.66358408284367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b93767777808cd5%3A0x43dec6621ae9fa98!2sAlexandra%20Headlands%20Hotel!5e0!3m2!1sen!2sau!4v1683160096446!5m2!1sen!2sau",
+    googleUrl: "https://goo.gl/maps/XD3f1fEGqxemgtXF6",
     appleUrl:
       "https://maps.apple.com/?address=98%20Alexandra%20Pde,%20Alexandra%20Headland%20QLD%204572,%20Australia&auid=2239366138665645170&ll=-26.663732,153.104705&lsp=9902&q=Alexandra%20Headlands%20Hotel",
     location:
       "Alexandra Headlands Hotel, 98 Alexandra Pde, Alexandra Headland QLD 4572",
+    image: alexHotel,
   },
 };
