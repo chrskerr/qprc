d2af6e0aeb6e8ab2aae1eb7094d3c58f246b542d
Chris Kerr <chriskerr@me.com>
2023-05-09 11:40:36 +1000

    added email address contact

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,7 @@ export const longTitle =
 export const metaDescription = `${shortTitle} is a free social running group based in Maroochydore, Queensland. Come join us for a run and maybe a drink after!`;
 export const socialImageLink: `https://${string}` =
   "https://www.qprc.com.au/full-logo.png";
+
+export const emailAddress = "quickle.pickle@icloud.com";
+export const signalJoinLink =
+  "https://signal.group/#CjQKIGlqYGaisbj7IpkudO8s9suHA4-nUW0rv6sfgH51TWUzEhAyF6Zzd6Bc-861TpbwTt8z";
