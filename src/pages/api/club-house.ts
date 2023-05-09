09837a660772ac99e85e38a43b913df53d8945b4
Chris Kerr <chriskerr@me.com>
2023-05-09 18:46:57 +1000

    start of chat

---
+++ src/pages/api/club-house.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from "astro";
+
+let callcount = 0;
+
+export const get: APIRoute = () => {
+  return {
+    body: JSON.stringify({
+      message: `This function has been called ${callcount} times`,
+    }),
+  };
+};
