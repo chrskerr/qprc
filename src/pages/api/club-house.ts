bd0f699e846db7e9e77aa5be0f6e3880cf98ee70
Chris Kerr <chriskerr@me.com>
2023-05-09 19:07:06 +1000

    testing simpler approach

--- src/pages/api/club-house.ts
+++ src/pages/api/club-house.ts
@@ -1,21 +1,25 @@
 import type { APIRoute } from "astro";
 
-let callcount = 0;
+import NodeCache from "node-cache";
 
-export const get: APIRoute = () => {
+const cache = new NodeCache({ stdTTL: 60 });
+
+export const get: APIRoute = async ({ request }) => {
+  const url = new URL(request.url);
+
+  if (url.searchParams.has("c")) {
+    cache.set("test", (Number(cache.get("test")) || 0) + 10);
+
+    return {
+      body: JSON.stringify({
+        message: "Added 10!",
+      }),
+    };
+  }
+
   return {
     body: JSON.stringify({
-      message: `This function has been called ${callcount++} times`,
-    }),
-  };
-};
-
-export const post: APIRoute = () => {
-  callcount += 10;
-
-  return {
-    body: JSON.stringify({
-      message: "This was a POST!",
+      message: cache.get("test"),
     }),
   };
 };
