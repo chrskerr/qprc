c9e214c58e72318fc87dfd4a1d798dfa2b71d92a
Chris Kerr <chriskerr@me.com>
2023-05-11 15:46:26 +1000

    don't show historical events in cal feed

--- src/pages/cal.ics.ts
+++ src/pages/cal.ics.ts
@@ -1,20 +1,22 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
-import { EventAttributes, ReturnObject, createEvents } from "ics";
+import { EventAttributes, createEvents } from "ics";
 import { getIcsDateArray } from "../lib/dates";
 import { longTitle } from "../lib/constants";
 import { meetingPoints } from "../lib/meetingPoints";
 
-let events: ReturnObject;
-
 async function generateIcs() {
   const runs = await getCollection("runs");
+  const now = Date.now();
 
   return createEvents(
     runs
       .map<EventAttributes | null>((run) => {
         const meetingPoint = meetingPoints[run.data.meetingPoint];
         if (!meetingPoint) return null;
+
+        // filter historical events
+        if (run.data.startTime.valueOf() < now) return null;
 
         return {
           uid: run.slug,
@@ -34,14 +36,12 @@ async function generateIcs() {
 }
 
 export const get: APIRoute = async () => {
-  if (!events) {
-    events = await generateIcs();
-  }
+  const events = await generateIcs();
 
   return new Response(events?.value, {
     headers: new Headers({
       "content-type": "text/calendar",
-      "cache-control": "public, max-age=86400, must-revalidate",
+      "cache-control": "public, max-age=3600, must-revalidate",
     }),
   });
 };
