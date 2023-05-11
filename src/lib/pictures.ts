823eee80b866dec52bbc89baf742747a9d463d8b
Chris Kerr <chriskerr@me.com>
2023-05-11 14:37:18 +1000

    added photos of us to home page

---
+++ src/lib/pictures.ts
@@ -0,0 +1,4 @@
+export { default as chris } from "../images/chris_headshot.png?w=400;800&format=avif;webp&as=picture";
+export { default as kate } from "../images/kate_headshot.png?w=400;800&format=avif;webp&as=picture";
+export { default as alexHill } from "../images/alex-hill-bottom.png?w=400;800;1200&format=avif;webp&as=picture";
+export { default as logo } from "../images/base-logo.png?w=300;600;900&format=avif;webp&as=picture";
